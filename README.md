7b1766f0c228ca9270c74db4894882fbe53af66f
coderaulia <auliasw89@gmail.com>
2021-08-10 21:41:01 +0700

    hello!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Uni-WP Theme
+
+Hi! It's my first time tryin to build a Wordpress Theme! I know it would be a long journey since I just started to learn it. But I think it would be a nice experience!
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo "<h1>Testing new theme</h1>";
+
+
+?>
+
+<h1>Hello World</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta facilis corrupti maxime minus optio corporis?</p>
+
+<a href="http://google.com">Google</a>
