541f30cdd280c9c9272fe2535f77072fde5f693a
coderaulia <auliasw89@gmail.com>
2021-08-27 22:47:30 +0700

    displaying custom post

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -2,7 +2,10 @@
 
 function university_post_types(){
    register_post_type('event', array(
+     'rewrite' => array('slug' => 'events'),
+     'has_archive' => true,
      'public' => true,
+     'show_in_rest' => true,
      'labels' => array(
        'name' => 'Events',
        'add_new_item' => 'Add New Event',
