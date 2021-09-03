0a10cc7155b791e44de888b456cfc4a1cf75148c
coderaulia <auliasw89@gmail.com>
2021-09-04 04:30:38 +0700

    adding search loading spinner and some prevention

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -10,6 +10,8 @@ class Search {
 		this.events();
 		// session to check the status of overlay
 		this.isOverlay = false;
+		this.isSpinnerVisible = false;
+		this.previousValue;
 		this.typingTimer;
 	}
 
@@ -17,21 +19,44 @@ class Search {
 		this.openButton.on("click", this.openOverlay.bind(this));
 		this.closeButton.on("click", this.closeOverlay.bind(this));
 		$(document).on("keydown", this.keyPressSearch.bind(this));
-		this.searchTerm.on("keydown", this.typingLogic.bind(this));
+		this.searchTerm.on("keyup", this.typingLogic.bind(this));
 	}
 
 	// Methods
 
 	typingLogic() {
-		clearTimeout(this.typingTimer); // clearing timeout until 2000ms
-		this.typingTimer = setTimeout(function () {
-			console.log("test");
-		}, 2000);
+		if (this.searchTerm.val() != this.previousValue) {
+			clearTimeout(this.typingTimer); // clearing timeout until 2000ms
+
+			// checking if search term not empty
+			if (this.searchTerm.val()) {
+				//adding spinner and checking current spinner state
+				if (!this.isSpinnerVisible) {
+					this.resultsDiv.html('<div class="spinner-loader"></div>');
+					this.isSpinnerVisible = true;
+				}
+				this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+			} else {
+				this.resultsDiv.html("");
+				this.isSpinnerVisible = false;
+			}
+		}
+		// get value
+		this.previousValue = this.searchTerm.val();
+	}
+
+	getResults() {
+		this.resultsDiv.html("");
+		this.isSpinnerVisible = false;
 	}
 
 	// trigger the keybinding for search and close the orverlay
 	keyPressSearch(e) {
-		if (e.keyCode == 83 && this.isOverlay == false) {
+		if (
+			e.keyCode == 83 &&
+			this.isOverlay == false &&
+			!$("input, textarea").is(":focus")
+		) {
 			this.openOverlay();
 		}
 		if (e.keyCode == 27 && this.isOverlay == true) {
